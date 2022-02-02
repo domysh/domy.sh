81b8307ceab5861079cacdf8ca6d285051810fe5
DomySh <me@domysh.com>
2022-02-03 00:01:35 +0100

    Head tags remodulated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -10,6 +10,8 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         <EmojiRender>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                <meta itemprop="image" content="/favicon.ico" />
+                <link itemprop="image" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
             </Head>
             <OverlayProvider>
