db92f00074a4f54eaf1b7967ea3a9a564ae4bc09
Domingo Dirutigliano <me@domy.sh>
2024-03-29 17:42:59 +0100

    twemoji to cloduflare cdn + update bun

--- modules/EmojiRender/index.jsx
+++ modules/EmojiRender/index.jsx
@@ -1,8 +1,7 @@
-import Twemoji from "react-twemoji"
-
+import Twemoji from "../Twemoji"
 export const EmojiRender = ({children}) => {
     return (
-        <Twemoji options={{ className: 'twemoji', folder: 'svg', ext: '.svg'}}>
+        <Twemoji options={{ className: 'twemoji', folder: 'svg', ext: '.svg', base:"https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/"}}>
             {children}
         </Twemoji>
     )
