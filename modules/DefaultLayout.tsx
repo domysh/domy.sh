4490d29e3eeb893dd0494a68ab02beffedacc63c
Domingo Dirutigliano <me@domy.sh>
2026-04-21 18:55:15 +0200

    Removing plausible

--- modules/DefaultLayout.tsx
+++ modules/DefaultLayout.tsx
@@ -10,7 +10,6 @@ export function DefaultLayout({ children }: { children: any }) {
         <NavBar />
         <Header />
         <Spacer />
-        <script defer data-domain="domy.sh" src="https://plausible.domy.sh/js/script.js"></script>
         <Container>
             {children}
         </Container>
